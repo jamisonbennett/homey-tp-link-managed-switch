d1f7fad64ac05002902d8abbc33e971b4ca2fcd8
Jamison Bennett <jamison.bennett@gmail.com>
2026-04-12 13:56:52 -0500

    Fix CVEs and update package
    Fix CVEs and update package.

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,15 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  modulePathIgnorePatterns: ['<rootDir>/.homeybuild/'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json',
+      },
+    ],
+  },
 };
 
 export default config;
